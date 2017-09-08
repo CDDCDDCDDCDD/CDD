3492dedfc800c1ca1d3bf6b656ff473b5432e218
CDD <378212718@qq.com>
2017-09-08 23:00:46 +0800

    测试上传

---
+++ Hellojava/src/com/demo/Hello.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Hello {
+
+	public void A(int a, int b, int c) {
+		System.out.println(a + "," + b + "," + c);
+	}
+
+	public static void main(String[] args) {
+		Hello a = new Hello();
+
+		a.A(544, 68465, 654654);
+
+	}
+
+}
